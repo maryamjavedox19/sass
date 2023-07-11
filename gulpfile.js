14c4aeba2ccca6202494a1e60f5770063865d3f2
Maryam <maryamjaved.xo@gmail.com>
2023-07-12 00:58:30 +0500

    partials

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss')
+  return src('*.scss')
     .pipe(sass())   //to compiled into css file
     .pipe(dest('css'))  //going to compile it and out css file and css folder
 }
 
 // when we make changes its going to automatically run buildstyle function
 function watchTask() {
-  watch(['index.scss'], buildStyles)  //1st arg array of files to watch, 2nd arg function we want to run when file changes
+  watch(['*.scss'], buildStyles)  //1st arg array of files to watch, 2nd arg function we want to run when file changes
 }
 
 exports.default = series(buildStyles, watchTask)   //to export them in order
