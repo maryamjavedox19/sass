ca0caa1598d9c6c8683c912dfaf954bb884f5bd7
Maryam <maryamjaved.xo@gmail.com>
2023-07-12 09:00:25 +0500

    nested rules

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,15 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('*.scss')
+  return src('shinobi/**/*.scss')    //**=look for subfolder */
     .pipe(sass())   //to compiled into css file
     .pipe(dest('css'))  //going to compile it and out css file and css folder
 }
 
 // when we make changes its going to automatically run buildstyle function
 function watchTask() {
-  watch(['*.scss'], buildStyles)  //1st arg array of files to watch, 2nd arg function we want to run when file changes
+  watch(['shinobi/**/*.scss'], buildStyles)
+  //1st arg array of files to watch, 2nd arg function we want to run when file changes
 }
 
 exports.default = series(buildStyles, watchTask)   //to export them in order
