d24c16b62f23b98bb85c6f949fdfa3bffe6177c7
Maryam <maryamjaved.xo@gmail.com>
2023-07-14 23:56:19 +0500

    customizing library

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,21 @@ const purgecss = require('gulp-purgecss')
 
 
 function buildStyles() {
-  return src('shinobi/**/*.scss')    //**=look for subfolder */
+
+  return src('sass/**/*.scss')    //**=look for subfolder */
+
     .pipe(sass())   //to compiled into css file
-    .pipe(purgecss({ content: ['*.html'] }))   //it will look for any public html file and see what css is using in connected
-    //  css file if theres some extra css which is not being used in html then it will remove it
+
+    .pipe(purgecss({ content: ['*.html'] }))       //2nd arg is for watch changes in html file so it could add new class in css file accordingly
+
+    //it will look for any public html file and see what css is using in connected css file if theres some extra css which is not being used in html then it will remove it
     .pipe(dest('css'))  //going to compile it and out css file and css folder
 }
 
 // when we make changes its going to automatically run buildstyle function
+
 function watchTask() {
-  watch(['shinobi/**/*.scss'], buildStyles)
+  watch(['sass/**/*.scss'], buildStyles)
   //1st arg array of files to watch, 2nd arg function we want to run when file changes
 }
 
