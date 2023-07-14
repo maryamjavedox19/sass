e4c7f2c46cece6c0c3161a8f8e6e808318a8aef5
Maryam <maryamjaved.xo@gmail.com>
2023-07-14 23:06:21 +0500

    purging css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const { src, dest, watch, series } = require('gulp')
 // src to  sass source file, dest to css outpul file destination
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
+
 
 function buildStyles() {
   return src('shinobi/**/*.scss')    //**=look for subfolder */
     .pipe(sass())   //to compiled into css file
+    .pipe(purgecss({ content: ['*.html'] }))   //it will look for any public html file and see what css is using in connected
+    //  css file if theres some extra css which is not being used in html then it will remove it
     .pipe(dest('css'))  //going to compile it and out css file and css folder
 }
 
